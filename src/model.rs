3823440aee9ebe8c233ba7f661255ab58d599825
Bertrand Darbon <bertrand.darbon@gmail.com>
2024-09-04 12:18:57 +0200

    🎉 Init datamodel (#59)

---
+++ src/model.rs
@@ -0,0 +1,34 @@
+use rand::{distributions::Alphanumeric, Rng};
+use tokio::time::Instant; // 0.8
+
+#[derive(Debug)]
+pub struct Transaction {
+    pub inner: String,
+}
+
+#[derive(Debug)]
+pub struct Block {
+    pub parent_hash: String,
+    pub height: usize,
+    pub timestamp: Instant,
+    pub txs: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn hash_block(&self) -> String {
+        rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect()
+    }
+
+    pub fn genesis() -> Block {
+        Block {
+            parent_hash: "000".to_string(),
+            height: 0,
+            timestamp: Instant::now(),
+            txs: vec![],
+        }
+    }
+}
