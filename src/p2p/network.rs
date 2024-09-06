6556f29f677101815e1b62fb682289b148297f7d
Bertrand Darbon <bertrand@hyle.eu>
2024-09-06 09:17:54 +0200

    ⚰️  Remove dead code

--- src/p2p/network.rs
+++ src/p2p/network.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
-use tokio::sync::mpsc::Sender;
 
-use crate::{ctx::CtxCommand, model::Transaction};
+use crate::model::Transaction;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Version {
@@ -20,17 +19,5 @@ pub enum NetMessage {
 impl NetMessage {
     pub fn as_binary(&self) -> Vec<u8> {
         bincode::serialize(&self).expect("Could not serialize NetMessage")
-    }
-
-    pub async fn handle(self, ctx: &Sender<CtxCommand>) {
-        match self {
-            NetMessage::Ping => todo!(),
-            NetMessage::Pong => todo!(),
-            NetMessage::NewTransaction(tx) => {
-                ctx.send(CtxCommand::AddTransaction(tx)).await.unwrap();
-            }
-            NetMessage::Version(_) => todo!(),
-            NetMessage::Verack => todo!(),
-        }
     }
 }
