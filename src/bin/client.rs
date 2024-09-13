5a491ab54be2444fc99ea6eedbdcde744b72e5f9
Bertrand Darbon <bertrand@hyle.eu>
2024-09-13 12:36:04 +0200

    ✨ CLI to send transactions to node

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,64 +1,145 @@
 use anyhow::{Context, Result};
-use clap::Parser;
+use clap::{command, Args, Parser, Subcommand};
+use config::{Config, ConfigError, Environment, File};
 use hyle::{
-    model::{Transaction, TransactionData},
+    model::{
+        BlobTransaction, ProofTransaction, RegisterContractTransaction, Transaction,
+        TransactionData,
+    },
     p2p::network::{MempoolNetMessage, NetMessage},
     utils::conf::{self, SharedConf},
 };
-use rand::{distributions::Alphanumeric, Rng};
-use tokio::{io::AsyncWriteExt, net::TcpStream, time::Duration};
+use serde::de::Deserialize;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 use tracing::info;
 
-pub fn new_transaction() -> Vec<u8> {
-    NetMessage::MempoolMessage(MempoolNetMessage::NewTx(Transaction {
-        inner: rand::thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(7)
-            .map(char::from)
-            .collect(),
-        version: 1,
-        transaction_data: TransactionData::default(),
-    }))
-    .to_binary()
+fn load<'de, T: Deserialize<'de>>(file: String) -> Result<T, ConfigError> {
+    let s = Config::builder()
+        .add_source(File::with_name(file.as_str()))
+        .add_source(Environment::with_prefix("hyle"))
+        .build()?;
+
+    s.try_deserialize::<T>()
 }
 
-pub async fn client(config: SharedConf) -> Result<()> {
-    let mut socket = TcpStream::connect(config.addr())
-        .await
-        .context("connecting to server")?;
-    info!("Starting client");
-    loop {
-        info!("Sending a message");
-        let res = new_transaction();
-        socket
-            .write_u32(res.len() as u32)
-            .await
-            .context("sending tcp message size")?;
-        socket
-            .write(res.as_ref())
-            .await
-            .context("sending tcp message")?;
-        tokio::time::sleep(Duration::from_secs(1)).await;
+fn to_net_message(tx: Transaction) -> Vec<u8> {
+    NetMessage::MempoolMessage(MempoolNetMessage::NewTx(tx)).to_binary()
+}
+
+fn wrap_tx(tx: TransactionData) -> Transaction {
+    Transaction {
+        version: 1,
+        transaction_data: tx,
+        inner: "".to_string(),
     }
 }
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
+fn load_blob(file: String) -> Result<Transaction> {
+    let blob = load::<BlobTransaction>(file).context("loading blob tx from file")?;
+    Ok(wrap_tx(TransactionData::Blob(blob)))
+}
+fn load_proof(file: String) -> Result<Transaction> {
+    let blob = load::<ProofTransaction>(file).context("loading blob tx from file")?;
+    Ok(wrap_tx(TransactionData::Proof(blob)))
+}
+fn load_contract(file: String) -> Result<Transaction> {
+    let blob = load::<RegisterContractTransaction>(file).context("loading blob tx from file")?;
+    Ok(wrap_tx(TransactionData::RegisterContract(blob)))
+}
+
+fn handle_send(send: SendArgs) -> Result<Vec<u8>> {
+    let tx = match send.command {
+        SendCommands::Blob { file } => load_blob(file)?,
+        SendCommands::B { file } => load_blob(file)?,
+        SendCommands::Proof { file } => load_proof(file)?,
+        SendCommands::P { file } => load_proof(file)?,
+        SendCommands::Contract { file } => load_contract(file)?,
+        SendCommands::C { file } => load_contract(file)?,
+    };
+    info!("Sending tx {:?}", tx);
+    Ok(to_net_message(tx))
+}
+
+fn handle_cli(cli: Cli) -> Result<Vec<u8>> {
+    match cli.command {
+        Commands::Send(s) => handle_send(s),
+        Commands::S(s) => handle_send(s),
+    }
+}
+
+async fn client(config: SharedConf, cli: Cli) -> Result<()> {
+    let res = handle_cli(cli)?;
+
+    let mut socket = TcpStream::connect(config.addr())
+        .await
+        .context("connecting to server")?;
+    socket
+        .write_u32(res.len() as u32)
+        .await
+        .context("sending tcp message size")?;
+    socket
+        .write(res.as_ref())
+        .await
+        .context("sending tcp message")?;
+
+    info!("Done");
+    Ok(())
+}
+
+/// A fictional versioning CLI
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(name = "hyle")]
+#[command(about = "A CLI to interact with hyle node", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
     #[arg(long, default_value = "master.ron")]
     config_file: String,
 }
 
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Send transactions to node
+    #[command(arg_required_else_help = true)]
+    Send(SendArgs),
+    /// Send transactions to node
+    S(SendArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = false)]
+struct SendArgs {
+    #[command(subcommand)]
+    command: SendCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum SendCommands {
+    /// Send blob transaction
+    Blob { file: String },
+    /// Send blob transaction
+    B { file: String },
+    /// Send proof transaction
+    Proof { file: String },
+    /// Send proof transaction
+    P { file: String },
+    /// Register contract
+    Contract { file: String },
+    /// Register contract
+    C { file: String },
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::parse();
+    let cli = Cli::parse();
 
     // install global collector configured based on RUST_LOG env var.
     tracing_subscriber::fmt::init();
 
-    let config = conf::Conf::new_shared(args.config_file)?;
+    let config = conf::Conf::new_shared(cli.config_file.clone())?;
     info!("Starting client with config: {:?}", config);
 
-    info!("client mode");
-    client(config).await
+    client(config, cli).await
 }
