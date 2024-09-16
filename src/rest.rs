2313d6506828f16ffcee150cf6b27c43355142e0
Bertrand Darbon <bertrand@hyle.eu>
2024-09-16 13:09:43 +0200

    ✨ Send tx from rest http

--- src/rest.rs
+++ src/rest.rs
@@ -1,17 +1,28 @@
-use crate::{indexer::Indexer, utils::conf::SharedConf};
+use crate::{bus::SharedMessageBus, indexer::Indexer, utils::conf::SharedConf};
 use anyhow::{Context, Result};
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use model::RouterState;
 use tracing::info;
 
 mod endpoints;
 pub mod model;
 
-pub async fn rest_server(config: SharedConf, idxr: Indexer) -> Result<()> {
+pub async fn rest_server(config: SharedConf, bus: SharedMessageBus, idxr: Indexer) -> Result<()> {
     info!("rest listening on {}", config.rest_addr());
     let app = Router::new()
-        .route("/getTransaction/:tx_hash", get(endpoints::get_transaction))
-        .route("/getBlock/:height", get(endpoints::get_block))
-        .with_state(idxr);
+        .route(
+            "/v1/contract/register",
+            post(endpoints::send_contract_transaction),
+        )
+        .route("/v1/tx/send/blob", post(endpoints::send_blob_transaction))
+        .route("/v1/tx/send/proof", post(endpoints::send_proof_transaction))
+        .route("/v1/tx/get/:tx_hash", get(endpoints::get_transaction))
+        .route("/v1/block/height/:height", get(endpoints::get_block))
+        .route("/v1/block/current", get(endpoints::get_current_block))
+        .with_state(RouterState { bus, idxr });
 
     let listener = tokio::net::TcpListener::bind(config.rest_addr())
         .await
--- src/rest/model.rs
+++ src/rest/model.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::{bus::SharedMessageBus, indexer::Indexer};
+
 #[derive(Serialize, Deserialize)]
 pub struct TransactionRequest {
     pub tx_hash: String,
@@ -8,4 +10,18 @@ pub struct TransactionRequest {
 #[derive(Serialize, Deserialize)]
 pub struct BlockRequest {
     pub block_number: u64,
+}
+
+pub struct RouterState {
+    pub bus: SharedMessageBus,
+    pub idxr: Indexer,
+}
+
+impl Clone for RouterState {
+    fn clone(&self) -> Self {
+        Self {
+            bus: self.bus.new_handle(),
+            idxr: self.idxr.clone(),
+        }
+    }
 }
