ef98f44b02da0ef5506d77a0449c40b9d3b35fd4
Alexandre Careil <alex.careil.fr@gmail.com>
2024-09-26 15:11:43 +0200

    wire client and add auto mode that generates calls

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,21 +1,14 @@
+use std::{str::FromStr, time::Duration};
+
 use anyhow::{Context, Result};
-use clap::{command, Args, Parser, Subcommand};
+use clap::{command, Parser, Subcommand};
 use config::{Config, ConfigError, Environment, File};
-use hyle::{
-    mempool::MempoolNetMessage,
-    model::{
-        BlobTransaction, ProofTransaction, RegisterContractTransaction, Transaction,
-        TransactionData,
-    },
-    p2p::{
-        network::{NetMessage, Signed},
-        stream::send_net_message,
-    },
-    utils::conf::{self, SharedConf},
-};
+use hyle::model::{BlobTransaction, ProofTransaction, RegisterContractTransaction};
+use rand::Rng;
+use reqwest::Url;
 use serde::de::Deserialize;
-use tokio::net::TcpStream;
-use tracing::info;
+use tokio::time::sleep;
+use tracing::{error, info};
 
 fn load<'de, T: Deserialize<'de>>(file: String) -> Result<T, ConfigError> {
     let s = Config::builder()
@@ -26,62 +19,51 @@ fn load<'de, T: Deserialize<'de>>(file: String) -> Result<T, ConfigError> {
     s.try_deserialize::<T>()
 }
 
-fn wrap_net_message(tx: Transaction) -> NetMessage {
-    NetMessage::MempoolMessage(Signed {
-        msg: MempoolNetMessage::NewTx(tx),
-        signature: Default::default(),
-        validators: Default::default(),
-    })
+struct ApiHttpClient {
+    url: Url,
+    reqwest_client: reqwest::Client,
 }
 
-fn wrap_tx(tx: TransactionData) -> Transaction {
-    Transaction::wrap(tx)
-}
+impl ApiHttpClient {
+    pub async fn send_tx_blob(&self, tx: &BlobTransaction) -> Result<String> {
+        let res = self
+            .reqwest_client
+            .post(format!("{}v1/tx/send/blob", self.url))
+            .body(serde_json::to_string(tx)?)
+            .header("Content-Type", "application/json")
+            .send()
+            .await
+            .context("Sending tx blob")?;
 
-fn load_blob(file: String) -> Result<Transaction> {
-    let blob = load::<BlobTransaction>(file).context("loading blob tx from file")?;
-    Ok(wrap_tx(TransactionData::Blob(blob)))
-}
-fn load_proof(file: String) -> Result<Transaction> {
-    let blob = load::<ProofTransaction>(file).context("loading blob tx from file")?;
-    Ok(wrap_tx(TransactionData::Proof(blob)))
-}
-fn load_contract(file: String) -> Result<Transaction> {
-    let blob = load::<RegisterContractTransaction>(file).context("loading blob tx from file")?;
-    Ok(wrap_tx(TransactionData::RegisterContract(blob)))
-}
-
-fn handle_send(send: SendArgs) -> Result<NetMessage> {
-    let tx = match send.command {
-        SendCommands::Blob { file } => load_blob(file)?,
-        SendCommands::B { file } => load_blob(file)?,
-        SendCommands::Proof { file } => load_proof(file)?,
-        SendCommands::P { file } => load_proof(file)?,
-        SendCommands::Contract { file } => load_contract(file)?,
-        SendCommands::C { file } => load_contract(file)?,
-    };
-    info!("Sending tx {:?}", tx);
-    Ok(wrap_net_message(tx))
-}
-
-fn handle_cli(cli: Cli) -> Result<NetMessage> {
-    match cli.command {
-        Commands::Send(s) => handle_send(s),
-        Commands::S(s) => handle_send(s),
+        res.text().await.context("test")
     }
-}
+    pub async fn send_tx_proof(&self, tx: &ProofTransaction) -> Result<String> {
+        let res = self
+            .reqwest_client
+            .post(format!("{}v1/tx/send/proof", self.url))
+            .body(serde_json::to_string(&tx)?)
+            .header("Content-Type", "application/json")
+            .send()
+            .await
+            .context("Sending tx proof")?;
 
-async fn client(config: SharedConf, cli: Cli) -> Result<()> {
-    let res = handle_cli(cli)?;
+        res.text().await.context("test")
+    }
+    pub async fn send_tx_register_contract(
+        &self,
+        tx: &RegisterContractTransaction,
+    ) -> Result<String> {
+        let res = self
+            .reqwest_client
+            .post(format!("{}v1/contract/register", self.url))
+            .body(serde_json::to_string(&tx)?)
+            .header("Content-Type", "application/json")
+            .send()
+            .await
+            .context("Sending tx register contract")?;
 
-    let mut socket = TcpStream::connect(config.addr())
-        .await
-        .context("connecting to server")?;
-
-    send_net_message(&mut socket, res).await?;
-
-    info!("Done");
-    Ok(())
+        res.text().await.context("test")
+    }
 }
 
 /// A cli to interact with hyle node
@@ -90,42 +72,24 @@ async fn client(config: SharedConf, cli: Cli) -> Result<()> {
 #[command(about = "A CLI to interact with hyle node", long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Commands,
-
-    #[arg(long, default_value = "master.ron")]
-    config_file: String,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// Send transactions to node
-    Send(SendArgs),
-    /// Send transactions to node
-    S(SendArgs),
-}
-
-#[derive(Debug, Args)]
-#[command(args_conflicts_with_subcommands = true)]
-#[command(flatten_help = false)]
-struct SendArgs {
-    #[command(subcommand)]
     command: SendCommands,
 }
 
 #[derive(Debug, Subcommand)]
 enum SendCommands {
     /// Send blob transaction
-    Blob { file: String },
-    /// Send blob transaction
-    B { file: String },
+    Blob {
+        file: String,
+    },
     /// Send proof transaction
-    Proof { file: String },
-    /// Send proof transaction
-    P { file: String },
+    Proof {
+        file: String,
+    },
     /// Register contract
-    Contract { file: String },
-    /// Register contract
-    C { file: String },
+    Contract {
+        file: String,
+    },
+    Auto,
 }
 
 #[tokio::main]
@@ -135,7 +99,66 @@ async fn main() -> Result<()> {
     // install global collector configured based on RUST_LOG env var.
     tracing_subscriber::fmt::init();
 
-    let config = conf::Conf::new_shared(cli.config_file.clone(), None)?;
+    let api_client = ApiHttpClient {
+        url: Url::from_str("http://localhost:4321").unwrap(),
+        reqwest_client: reqwest::Client::new(),
+    };
 
-    client(config, cli).await
+    match cli.command {
+        SendCommands::Blob { file } => {
+            let blob = load::<BlobTransaction>(file).context("loading blob tx from file")?;
+            let res = api_client.send_tx_blob(&blob).await?;
+            info!("Received response {}", res);
+        }
+        SendCommands::Proof { file } => {
+            let blob = load::<ProofTransaction>(file).context("loading proof tx from file")?;
+            let res = api_client.send_tx_proof(&blob).await?;
+            info!("Received response {}", res);
+        }
+        SendCommands::Contract { file } => {
+            let blob = load::<RegisterContractTransaction>(file)
+                .context("loading contract tx from file")?;
+            let res = api_client.send_tx_register_contract(&blob).await?;
+            info!("Received response {}", res);
+        }
+        SendCommands::Auto => {
+            info!("Starting auto mode");
+
+            let tx_blob = load::<BlobTransaction>("./data/tx1_blob.ron".to_string())
+                .context("loading blob tx from file")?;
+            let tx_proof = load::<ProofTransaction>("./data/tx1_proof.ron".to_string())
+                .context("loading proof tx from file")?;
+            let tx_contract =
+                load::<RegisterContractTransaction>("./data/contract_c1.ron".to_string())
+                    .context("loading contract tx from file")?;
+
+            let mut rand = rand::thread_rng();
+
+            loop {
+                match rand.gen_range(1..4) {
+                    1 => {
+                        info!("Sending tx blob");
+                        _ = api_client.send_tx_blob(&tx_blob).await;
+                    }
+                    2 => {
+                        info!("Sending tx proof");
+                        _ = api_client.send_tx_proof(&tx_proof).await;
+                    }
+                    3 => {
+                        info!("Sending contract");
+                        _ = api_client.send_tx_register_contract(&tx_contract).await;
+                    }
+                    _ => {
+                        error!("unknown random choice");
+                    }
+                }
+
+                sleep(Duration::from_millis(500)).await;
+            }
+        }
+    }
+
+    info!("Done");
+
+    Ok(())
 }
