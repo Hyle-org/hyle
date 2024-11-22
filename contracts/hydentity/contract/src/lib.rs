f7b0c2cc321a963f82c14907a1737ea424f4a75e
Bertrand Darbon <bertrand@hyle.eu>
2024-11-22 12:57:09 +0100

    ⚡️ Use accelerated sha2 in risc0 VM (#336)
    * ⚡️ Use accelerated sha2 in risc0 VM
    
    https://dev.risczero.com/api/zkvm/acceleration
    
    * regenerate contracts & proofs
    
    * ✅ fix tests

--- contracts/hydentity/contract/src/lib.rs
+++ contracts/hydentity/contract/src/lib.rs
@@ -1,21 +1,21 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use anyhow::Error;
 use bincode::{Decode, Encode};
 use serde::{Deserialize, Serialize};
 
 use sdk::{identity_provider::IdentityVerification, Digestable, HyleContract};
-use sha3::{Digest, Sha3_256};
+use sha2::{Digest, Sha256};
 
 #[derive(Encode, Decode, Serialize, Deserialize, Debug, Clone)]
 pub struct Hydentity {
-    identities: HashMap<String, String>,
+    identities: BTreeMap<String, String>,
 }
 
 impl Hydentity {
     pub fn new() -> Self {
         Hydentity {
-            identities: HashMap::new(),
+            identities: BTreeMap::new(),
         }
     }
 }
@@ -39,7 +39,7 @@ impl IdentityVerification for Hydentity {
         private_input: &str,
     ) -> Result<(), &'static str> {
         let id = format!("{account}:{private_input}");
-        let mut hasher = Sha3_256::new();
+        let mut hasher = Sha256::new();
         hasher.update(id.as_bytes());
         let hash_bytes = hasher.finalize();
         self.identities
@@ -56,7 +56,7 @@ impl IdentityVerification for Hydentity {
         match self.identities.get(account) {
             Some(stored_info) => {
                 let id = format!("{account}:{private_input}");
-                let mut hasher = Sha3_256::new();
+                let mut hasher = Sha256::new();
                 hasher.update(id.as_bytes());
                 let hashed = hex::encode(hasher.finalize());
                 Ok(*stored_info == hashed && !blobs_hash.is_empty())
@@ -94,7 +94,7 @@ impl TryFrom<sdk::StateDigest> for Hydentity {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use sha3::{Digest, Sha3_256};
+    use sha2::{Digest, Sha256};
 
     #[test]
     fn test_register_identity() {
@@ -105,7 +105,7 @@ mod tests {
         assert!(hydentity.register_identity(account, private_input).is_ok());
 
         let id = format!("{account}:{private_input}");
-        let mut hasher = Sha3_256::new();
+        let mut hasher = Sha256::new();
         hasher.update(id.as_bytes());
         let hash_bytes = hasher.finalize();
         let expected_hash = hex::encode(hash_bytes);
@@ -142,7 +142,7 @@ mod tests {
         hydentity.register_identity(account, private_input).unwrap();
 
         let id = format!("{account}:{private_input}");
-        let mut hasher = Sha3_256::new();
+        let mut hasher = Sha256::new();
         hasher.update(id.as_bytes());
         let hash_bytes = hasher.finalize();
         let expected_hash = hex::encode(hash_bytes);
