fa227caec85747938a528df94491e19570810b49
maximilien-hyle <maximilien@hyle.eu>
2024-11-27 17:30:30 +0100

    feat: Allow swapping in other direction (y -> x)

--- contracts/amm/contract/src/lib.rs
+++ contracts/amm/contract/src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 
 use anyhow::Error;
 use bincode::{Decode, Encode};
@@ -9,6 +10,37 @@ use serde::Deserialize;
 type TokenPair = (String, String);
 type TokenPairAmount = (u128, u128);
 
+#[derive(Debug, Deserialize, Clone, Encode, Decode)]
+struct UnorderedTokenPair {
+    a: String,
+    b: String,
+}
+
+impl UnorderedTokenPair {
+    fn new(x: String, y: String) -> Self {
+        if x <= y {
+            UnorderedTokenPair { a: x, b: y }
+        } else {
+            UnorderedTokenPair { a: y, b: x }
+        }
+    }
+}
+
+impl PartialEq for UnorderedTokenPair {
+    fn eq(&self, other: &Self) -> bool {
+        self.a == other.a && self.b == other.b
+    }
+}
+
+impl Eq for UnorderedTokenPair {}
+
+impl Hash for UnorderedTokenPair {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.a.hash(state);
+        self.b.hash(state);
+    }
+}
+
 pub struct AmmContract {
     state: AmmState,
     caller: Identity,
@@ -16,7 +48,7 @@ pub struct AmmContract {
 
 #[derive(Deserialize, Clone, Encode, Decode)]
 pub struct AmmState {
-    pairs: HashMap<TokenPair, TokenPairAmount>,
+    pairs: HashMap<UnorderedTokenPair, TokenPairAmount>,
 }
 
 impl AmmContract {
@@ -159,21 +191,45 @@ impl AmmContract {
         };
 
         // Compute x,y and check swap is legit (x*y=k)
-        match self.state.pairs.get_mut(&pair) {
+        let normalized_pair = UnorderedTokenPair::new(pair.0.clone(), pair.1.clone());
+        let is_normalized_order = pair.0 <= pair.1;
+        match self.state.pairs.get_mut(&normalized_pair) {
             Some((prev_x, prev_y)) => {
-                if (*prev_x + from_amount) * (*prev_y - to_amount) != *prev_x * *prev_y {
-                    return RunResult {
-                        success: false,
-                        identity: self.caller.clone(),
-                        program_outputs: format!(
-                            "Swap formula is not respected: {} * {} != {} * {}",
-                            prev_x, prev_y, from_amount, to_amount
-                        )
-                        .into_bytes(),
-                    };
+                if is_normalized_order {
+                    if (*prev_x + from_amount) * (*prev_y - to_amount) != *prev_x * *prev_y {
+                        return RunResult {
+                            success: false,
+                            identity: self.caller.clone(),
+                            program_outputs: format!(
+                                "Swap formula is not respected: {} * {} != {} * {}",
+                                (*prev_x + from_amount),
+                                (*prev_y - to_amount),
+                                prev_x,
+                                prev_y
+                            )
+                            .into_bytes(),
+                        };
+                    }
+                    *prev_x += from_amount;
+                    *prev_y -= to_amount;
+                } else {
+                    if (*prev_y + from_amount) * (*prev_x - to_amount) != *prev_y * *prev_x {
+                        return RunResult {
+                            success: false,
+                            identity: self.caller.clone(),
+                            program_outputs: format!(
+                                "Swap formula is not respected: {} * {} != {} * {}",
+                                (*prev_y + from_amount),
+                                (*prev_x - to_amount),
+                                prev_y,
+                                prev_x
+                            )
+                            .into_bytes(),
+                        };
+                    }
+                    *prev_y += from_amount;
+                    *prev_x -= to_amount;
                 }
-                *prev_x += from_amount;
-                *prev_y -= to_amount;
             }
             None => {
                 return RunResult {
@@ -240,8 +296,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_success() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -259,19 +317,44 @@ mod tests {
         );
         assert!(result.success);
         // Assert that the amounts for the pair token1/token2 have been updated
-        assert!(
-            contract
-                .state
-                .pairs
-                .get(&("token1".to_string(), "token2".to_string()))
-                == Some(&(25, 40))
+        assert!(contract.state.pairs.get(&normalized_token_pair) == Some(&(25, 40)));
+    }
+
+    #[test]
+    fn test_verify_opposite_swap_success() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
+        let state = AmmState {
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
+        };
+
+        let caller = Identity("test".to_owned());
+        let mut contract = AmmContract::new(state, caller.clone());
+
+        // Swaping from token2 to token1
+        let blobs = vec![
+            create_test_blob("token2", "test", "amm", 50),
+            create_test_blob("token1", "amm", "test", 10),
+        ];
+
+        let result = contract.verify_swap(
+            blobs,
+            caller.clone(),
+            ("token2".to_string(), "token1".to_string()),
         );
+
+        assert!(result.success);
+        // Assert that the amounts for the pair token1/token2 have been updated
+        println!("{:?}", contract.state.pairs.get(&normalized_token_pair));
+        assert!(contract.state.pairs.get(&normalized_token_pair) == Some(&(10, 100)));
     }
 
     #[test]
     fn test_verify_swap_caller_mismatch() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
         let caller = Identity("test".to_owned());
         let mut contract = AmmContract::new(state, caller.clone());
@@ -292,8 +375,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_sender() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -314,8 +399,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_amm_sender() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -336,8 +423,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_recipient() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -358,8 +447,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_amm_recipient() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -380,8 +471,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_pair() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -402,8 +495,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_pair_in_blobs() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -424,8 +519,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_blob_not_found() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
@@ -443,8 +540,10 @@ mod tests {
 
     #[test]
     fn test_verify_swap_invalid_swap_formula() {
+        let normalized_token_pair =
+            UnorderedTokenPair::new("token1".to_string(), "token2".to_string());
         let state = AmmState {
-            pairs: HashMap::from([(("token1".to_string(), "token2".to_string()), (20, 50))]),
+            pairs: HashMap::from([(normalized_token_pair.clone(), (20, 50))]),
         };
 
         let caller = Identity("test".to_owned());
