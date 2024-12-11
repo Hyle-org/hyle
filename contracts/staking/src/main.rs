5d72f2bb6f4a2e9c8503888ac14ec5b3edbb653a
Bertrand Darbon <bertrand@hyle.eu>
2024-12-11 16:19:18 +0100

    fix workspace

--- contracts/staking/src/main.rs
+++ contracts/staking/src/main.rs
@@ -3,30 +3,6 @@
 
 extern crate alloc;
 
-use hyllar::{HyllarToken, HyllarTokenContract};
-use risc0_zkvm::guest::env;
-use sdk::erc20::ERC20Action;
-
 risc0_zkvm::guest::entry!(main);
 
-fn main() {
-    let (input, parsed_blob, caller) =
-        match sdk::guest::init_with_caller::<HyllarToken, ERC20Action>() {
-            Ok(res) => res,
-            Err(err) => {
-                panic!("Hyllar contract initialization failed {}", err);
-            }
-        };
-
-    let state = input.initial_state.clone();
-
-    env::log("Init token contract");
-    let mut contract = HyllarTokenContract::init(state, caller);
-
-    env::log("execute action");
-    let res = sdk::erc20::execute_action(&mut contract, parsed_blob.data.parameters);
-
-    env::log(alloc::format!("commit {:?}", res).as_str());
-
-    sdk::guest::commit(input, contract.state(), res);
-}
+fn main() {}
