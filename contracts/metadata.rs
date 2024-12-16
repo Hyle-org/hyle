3c8f29cc08a8553421e5d5f17798150d2eec72f4
Lancelot de Ferrière <wraitii@gmail.com>
2024-12-16 11:43:33 +0100

    ✨ Risc zero recursion program + hyrun update

--- contracts/metadata.rs
+++ contracts/metadata.rs
@@ -1,10 +1,10 @@
 #[allow(unused)]
-#[cfg(all(not(clippy), feature = "build"))]
+#[cfg(all(not(clippy), feature = "nonreproducible"))]
 mod methods {
     include!(concat!(env!("OUT_DIR"), "/methods.rs"));
 }
 
-#[cfg(all(not(clippy), feature = "build"))]
+#[cfg(all(not(clippy), feature = "nonreproducible"))]
 mod metadata {
     pub const AMM_ELF: &[u8] = crate::methods::AMM_ELF;
     pub const AMM_ID: [u8; 32] = sdk::to_u8_array(&crate::methods::AMM_ID);
@@ -17,9 +17,12 @@ mod metadata {
 
     pub const STAKING_ELF: &[u8] = crate::methods::STAKING_ELF;
     pub const STAKING_ID: [u8; 32] = sdk::to_u8_array(&crate::methods::STAKING_ID);
+
+    pub const RISC0_RECURSION_ELF: &[u8] = crate::methods::RISC0_RECURSION_ELF;
+    pub const RISC0_RECURSION_ID: [u8; 32] = sdk::to_u8_array(&crate::methods::RISC0_RECURSION_ID);
 }
 
-#[cfg(any(clippy, not(feature = "build")))]
+#[cfg(any(clippy, not(feature = "nonreproducible")))]
 mod metadata {
     pub const AMM_ELF: &[u8] = include_bytes!("amm/amm.img");
     pub const AMM_ID: [u8; 32] = sdk::str_to_u8(include_str!("amm/amm.txt"));
@@ -32,6 +35,10 @@ mod metadata {
 
     pub const STAKING_ELF: &[u8] = include_bytes!("staking/staking.img");
     pub const STAKING_ID: [u8; 32] = sdk::str_to_u8(include_str!("staking/staking.txt"));
+
+    pub const RISC0_RECURSION_ELF: &[u8] = include_bytes!("risc0-recursion/risc0-recursion.img");
+    pub const RISC0_RECURSION_ID: [u8; 32] =
+        sdk::str_to_u8(include_str!("risc0-recursion/risc0-recursion.txt"));
 }
 
 pub use metadata::*;
