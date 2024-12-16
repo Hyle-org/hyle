2e1f6e5d73c5e6f2ba26d35bacc9669789894f30
Lancelot de Ferrière <wraitii@gmail.com>
2024-12-16 11:43:33 +0100

    ✨ Risc zero recursion program + hyrun update

---
+++ contracts/risc0-recursion/src/lib.rs
@@ -0,0 +1,17 @@
+#![no_std]
+
+#[cfg(feature = "metadata")]
+pub mod metadata {
+    pub const RISC0_RECURSION_ELF: &[u8] = include_bytes!("../risc0-recursion.img");
+    pub const PROGRAM_ID: [u8; 32] = sdk::str_to_u8(include_str!("../risc0-recursion.txt"));
+}
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+pub struct ProofInput {
+    pub image_id: [u8; 32],
+    pub journal: Vec<u8>, // Should be a serde::to_vec<sdk::HyleOutput>,
+}
--- crates/hyrun/src/main.rs
+++ crates/hyrun/src/main.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
-use hyrun::{run_command, Cli};
+use hyrun::{run_command, Cli, Context, ContractData};
 
 fn main() {
     let cli = Cli::parse();
-    run_command(cli);
+    let context = Context {
+        cli,
+        contract_data: ContractData::default(),
+        hardcoded_initial_states: Default::default(),
+    };
+    run_command(&context);
 }
