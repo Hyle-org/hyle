12cc87b56a55a44202c11c088a210903a558aee6
Bertrand Darbon <bertrand@hyle.eu>
2025-01-02 11:41:23 +0100

    ♻️

--- crates/client-sdk/src/helpers.rs
+++ crates/client-sdk/src/helpers.rs
@@ -4,16 +4,26 @@ use sdk::{ContractInput, HyleOutput};
 use crate::ProofData;
 
 #[allow(dead_code)]
-fn prove(binary: Vec<u8>, input: ContractInput) -> Result<(ProofData, HyleOutput)> {
-    let env = risc0_zkvm::ExecutorEnv::builder()
-        .write(&input)
-        .unwrap()
-        .build()
-        .unwrap();
+pub async fn prove(
+    binary: &[u8],
+    contract_input: &ContractInput,
+) -> Result<(ProofData, HyleOutput)> {
+    let contract_input = bonsai_runner::as_input_data(&contract_input)?;
 
-    let prover = risc0_zkvm::default_prover();
+    let explicit = std::env::var("RISC0_PROVER").unwrap_or_default();
+    let receipt = match explicit.to_lowercase().as_str() {
+        "bonsai" => bonsai_runner::run_bonsai(binary, contract_input.clone()).await?,
+        _ => {
+            let env = risc0_zkvm::ExecutorEnv::builder()
+                .write_slice(&contract_input)
+                .build()
+                .unwrap();
 
-    let receipt = prover.prove(env, &binary).unwrap().receipt;
+            let prover = risc0_zkvm::default_prover();
+            let prove_info = prover.prove(env, binary)?;
+            prove_info.receipt
+        }
+    };
 
     let hyle_output = receipt
         .journal
@@ -22,13 +32,12 @@ fn prove(binary: Vec<u8>, input: ContractInput) -> Result<(ProofData, HyleOutput
 
     if !hyle_output.success {
         let program_error = std::str::from_utf8(&hyle_output.program_outputs).unwrap();
-        println!(
+        bail!(
             "\x1b[91mExecution failed ! Program output: {}\x1b[0m",
             program_error
         );
-        bail!("Execution failed");
     }
 
-    let proof = ProofData::Bytes(borsh::to_vec(&receipt).expect("Unable to encode receipt"));
-    Ok((proof, hyle_output))
+    let encoded_receipt = borsh::to_vec(&receipt).expect("Unable to encode receipt");
+    Ok((ProofData::Bytes(encoded_receipt), hyle_output))
 }
