a87ee5874890bc4f5346a4a4b1818ce981dbb38b
Bertrand Darbon <bertrand@hyle.eu>
2025-01-14 12:39:11 +0100

    ♻️  Use .into() to build Transaction instead of wrap() (#602)
    * ♻️  Use .into() to build Transaction instead of wrap()
    
    * clippy
    
    * make wrap private

--- src/genesis.rs
+++ src/genesis.rs
@@ -9,8 +9,7 @@ use crate::{
     mempool::DataProposal,
     model::{
         BlobProofOutput, BlobTransaction, Hashable, ProofData, RegisterContractTransaction,
-        SharedRunContext, SignedBlock, Transaction, TransactionData, ValidatorPublicKey,
-        VerifiedProofTransaction,
+        SharedRunContext, SignedBlock, Transaction, ValidatorPublicKey, VerifiedProofTransaction,
     },
     p2p::network::PeerEvent,
     utils::{
@@ -225,10 +224,10 @@ impl Genesis {
             let tx = BlobTransaction { identity, blobs };
             let blob_tx_hash = tx.hash();
 
-            genesis_txs.push(Transaction::wrap(TransactionData::Blob(tx)));
+            genesis_txs.push(tx.into());
 
             // Pretend we're verifying a recursive proof
-            genesis_txs.push(Transaction::wrap(TransactionData::VerifiedProof(
+            genesis_txs.push(
                 VerifiedProofTransaction {
                     contract_name: "risc0-recursion".into(),
                     proven_blobs: outputs
@@ -246,8 +245,9 @@ impl Genesis {
                     is_recursive: true,
                     proof_hash: ProofData::default().hash(),
                     proof: None,
-                },
-            )));
+                }
+                .into(),
+            );
         }
 
         Ok(genesis_txs)
@@ -393,42 +393,38 @@ impl Genesis {
         (
             map,
             vec![
-                Transaction::wrap(TransactionData::RegisterContract(
-                    RegisterContractTransaction {
-                        owner: "hyle".into(),
-                        verifier: "risc0".into(),
-                        program_id: staking_program_id.into(),
-                        state_digest: states.staking.on_chain_state().as_digest(),
-                        contract_name: "staking".into(),
-                    },
-                )),
-                Transaction::wrap(TransactionData::RegisterContract(
-                    RegisterContractTransaction {
-                        owner: "hyle".into(),
-                        verifier: "risc0".into(),
-                        program_id: hyllar_program_id.into(),
-                        state_digest: states.hyllar.as_digest(),
-                        contract_name: "hyllar".into(),
-                    },
-                )),
-                Transaction::wrap(TransactionData::RegisterContract(
-                    RegisterContractTransaction {
-                        owner: "hyle".into(),
-                        verifier: "risc0".into(),
-                        program_id: hydentity_program_id.into(),
-                        state_digest: states.hydentity.as_digest(),
-                        contract_name: "hydentity".into(),
-                    },
-                )),
-                Transaction::wrap(TransactionData::RegisterContract(
-                    RegisterContractTransaction {
-                        owner: "hyle".into(),
-                        verifier: "risc0".into(),
-                        program_id: hyle_contracts::RISC0_RECURSION_ID.to_vec().into(),
-                        state_digest: hyle_contract_sdk::StateDigest(vec![]),
-                        contract_name: "risc0-recursion".into(),
-                    },
-                )),
+                RegisterContractTransaction {
+                    owner: "hyle".into(),
+                    verifier: "risc0".into(),
+                    program_id: staking_program_id.into(),
+                    state_digest: states.staking.on_chain_state().as_digest(),
+                    contract_name: "staking".into(),
+                }
+                .into(),
+                RegisterContractTransaction {
+                    owner: "hyle".into(),
+                    verifier: "risc0".into(),
+                    program_id: hyllar_program_id.into(),
+                    state_digest: states.hyllar.as_digest(),
+                    contract_name: "hyllar".into(),
+                }
+                .into(),
+                RegisterContractTransaction {
+                    owner: "hyle".into(),
+                    verifier: "risc0".into(),
+                    program_id: hydentity_program_id.into(),
+                    state_digest: states.hydentity.as_digest(),
+                    contract_name: "hydentity".into(),
+                }
+                .into(),
+                RegisterContractTransaction {
+                    owner: "hyle".into(),
+                    verifier: "risc0".into(),
+                    program_id: hyle_contracts::RISC0_RECURSION_ID.to_vec().into(),
+                    state_digest: hyle_contract_sdk::StateDigest(vec![]),
+                    contract_name: "risc0-recursion".into(),
+                }
+                .into(),
             ],
             states,
         )
