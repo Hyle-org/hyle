fddf7c77a4de4d9650160aff41a6d3652bd9981a
Bertrand Darbon <bertrand@hyle.eu>
2025-02-07 09:53:24 +0100

    🐛 Fix Hyllar Indexer computing wrong caller (#749)
    * 🐛 Fix Hyllar Indexer computing wrong caller
    
    * clippy

--- src/indexer/contract_handlers.rs
+++ src/indexer/contract_handlers.rs
@@ -78,9 +78,13 @@ impl ContractHandler for HyllarToken {
 
         let data: StructuredBlobData<ERC20Action> = data.clone().try_into()?;
 
-        let caller: Identity = data
+        let caller = data
             .caller
-            .map(|_| contract_name.0.clone().into())
+            .and_then(|idx| {
+                tx.blobs
+                    .get(idx.0)
+                    .map(|b| Identity(b.contract_name.0.clone()))
+            })
             .unwrap_or(tx.identity.clone());
 
         let contract = HyllarTokenContract::init(state, caller);
