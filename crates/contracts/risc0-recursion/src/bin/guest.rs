685d933eb056316608faf4ef3e0872ae887de10c
Bertrand D. <bertrand@hyle.eu>
2025-03-04 17:51:51 +0100

    🔧 Fix contract build (#853)
    broken by #837

--- crates/contracts/risc0-recursion/src/bin/guest.rs
+++ crates/contracts/risc0-recursion/src/bin/guest.rs
@@ -4,13 +4,13 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
-use hyle_risc0_recursion::{Risc0Journal, Risc0ProgramId};
+use hyle_risc0_recursion::{ProofInput, Risc0Journal, Risc0ProgramId};
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
 fn main() {
-    let mut input: Vec<risc0_recursion::ProofInput> = env::read();
+    let mut input: Vec<ProofInput> = env::read();
     let mut outputs: Vec<(Risc0ProgramId, Risc0Journal)> = Vec::new();
     input.iter_mut().for_each(|input| {
         risc0_zkvm::guest::env::verify(input.image_id, &input.journal)
