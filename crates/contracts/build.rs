Hyle-org/hyle

commit 6ae8a045151be5d954b8ec7a98b74346f45ff485

🔧 Better usage of cargo workspace (#837)
* 🔧 Better usage of cargo workspace

prep work to publish on crates.io

* fix usage of sha2 precompile

* rename all package to start with "hyle-" + add description

* fix contracts

crates/contracts/risc0-recursion/src/bin/guest.rs
@@ -4,7 +4,7 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
-use risc0_recursion::{Risc0Journal, Risc0ProgramId};
+use hyle_risc0_recursion::{Risc0Journal, Risc0ProgramId};
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);

Cargo.toml
@@ -33,8 +33,24 @@ repository = "https://github.com/Hyle-org/hyle/"
 rust-version = "1.84"
 # licence = ""
 
+[workspace.dependencies]
+bonsai-runner = { version = "0.12.0", default-features = false, path = "crates/bonsai-runner", package = "hyle-bonsai-runner" }
+sdk = { version = "0.12.0", default-features = false, path = "crates/contract-sdk", package = "hyle-contract-sdk" }
+hyle-contract-sdk = { version = "0.12.0", default-features = false, path = "crates/contract-sdk", package = "hyle-contract-sdk" }
+client-sdk = { version = "0.12.0", default-features = false, path = "crates/client-sdk", package = "hyle-client-sdk" }
+hyle-contracts = { version = "0.12.0", default-features = false, path = "crates/contracts", package = "hyle-contracts" }
+hyle-model = { version = "0.12.0", default-features = false, path = "crates/hyle-model", package = "hyle-model" }
+hyle-verifiers = { version = "0.12.0", default-features = false, path = "crates/hyle-verifiers", package = "hyle-verifiers" }
+risc0-recursion = { version = "0.12.0", default-features = false, path = "crates/contracts/risc0-recursion", package = "hyle-risc0-recursion" }
+hydentity = { version = "0.12.0", default-features = false, path = "crates/contracts/hydentity", package = "hyle-hydentity" }
+hyllar = { version = "0.12.0", default-features = false, path = "crates/contracts/hyllar", package = "hyle-hyllar" }
+staking = { version = "0.12.0", default-features = false, path = "crates/contracts/staking", package = "hyle-staking" }
+amm = { version = "0.12.0", default-features = false, path = "crates/contracts/amm", package = "hyle-amm" }
+uuid-tld = { version = "0.12.0", default-features = false, path = "crates/contracts/uuid-tld", package = "hyle-uuid-tld" }
+
 [package]
 name = "hyle"
+description = "Hyle node"
 version = { workspace = true }
 edition = { workspace = true }
 # license = { workspace = true }
@@ -42,7 +58,6 @@ homepage = { workspace = true }
 repository = { workspace = true }
 rust-version = { workspace = true }
 default-run = "hyle"
-
 
 [[bin]]
 name = "hyle"
@@ -58,26 +73,25 @@ indexing_slicing = "warn"
 
 [dependencies]
 # Public dependencies
-hyle-contract-sdk = { path = "./crates/contract-sdk", default-features = false, features = [
+hyle-contract-sdk = { workspace = true, default-features = false, features = [
     "tracing",
 ] }
-staking = { path = "./crates/contracts/staking", default-features = false, features = [
+staking = { workspace = true, default-features = false, features = [
     "client",
 ] }
-client-sdk = { path = "./crates/client-sdk", default-features = false, features = [
-    "rest",
+client-sdk = { workspace = true, features = [
+  "rest",
     "tcp",
     "indexer"
 ] }
-hyle-model = { path = "./crates/hyle-model", default-features = false, features = [
+hyle-model = { workspace = true, default-features = false, features = [
     "full",
     "sqlx",
 ] }
-hydentity = { path = "./crates/contracts/hydentity", features = ["client"] }
-hyllar = { path = "./crates/contracts/hyllar", features = ["client"] }
-risc0-recursion = { path = "./crates/contracts/risc0-recursion" }
-
-hyle-verifiers = { path = "crates/hyle-verifiers" }
+hydentity = { workspace = true, features = ["client"] }
+hyllar = { workspace = true, features = ["client"] }
+risc0-recursion = { workspace = true }
+hyle-verifiers = { workspace = true }
 
 
 anyhow = "1.0.96"
@@ -100,7 +114,7 @@ bytes = { version = "1.10.0" }
 clap = { version = "4.5.31", features = ["derive"] }
 config = { version = "=0.15.0", default-features = false, features = ["ron"] }
 futures = { version = "0.3.31" }
-hyle-contracts = { path = "./crates/contracts", package = "hyle-contracts" }
+hyle-contracts = { workspace = true }
 indexmap = { version = "2.7.1", features = ["serde"] }
 # opentelemetry and axum-otel-metrics must be updated together (so that there is only one opentelemetry version)
 opentelemetry = { version = "0.28" }
@@ -134,10 +148,13 @@ utoipa-swagger-ui = { version = "9.0.0", features = ["axum"] }
 utoipa-axum = { version = "0.2.0" }
 testcontainers-modules = { version = "0.11.6", features = ["postgres"] }
 
+[patch.crates-io]
+sha2 = { git = "https://github.com/risc0/RustCrypto-hashes", tag = "sha2-v0.10.8-risczero.0" }
+
 [dev-dependencies]
-amm = { path = "./crates/contracts/amm", features = ["client"] }
-uuid-tld = { path = "./crates/contracts/uuid-tld", features = ["client"] }
-client-sdk = { path = "./crates/client-sdk", default-features = false, features = [
+amm = { workspace = true, features = ["client"] }
+uuid-tld = { workspace = true, features = ["client"] }
+client-sdk = { workspace = true, default-features = false, features = [
     "rest",
     "tcp",
     "risc0",
@@ -155,7 +172,7 @@ test-log = { version = "0.2.17", features = [
 tokio-stream = "0.1.17"
 tempfile = "3.17.1"
 assert-json-diff = "2.0.2"
-risc0-recursion = { path = "./crates/contracts/risc0-recursion" }
+risc0-recursion = { workspace = true }
 risc0-zkvm = { version = "1.2.4", default-features = false, features = [
     "client",
 ] }

crates/bonsai-runner/Cargo.toml
@@ -1,5 +1,6 @@
 [package]
-name = "bonsai-runner"
+name = "hyle-bonsai-runner"
+description = "Hyle async lib to use risc0 bonsai"
 version = { workspace = true }
 edition = { workspace = true }
 # license = { workspace = true }

crates/bonsai-runner/src/bin/bonsai-runner.rs
@@ -47,7 +47,7 @@ async fn prove_handler(
     }
 
     // Appeler la fonction `run_bonsai`
-    let receipt = bonsai_runner::run_bonsai(&payload.elf, payload.input_data)
+    let receipt = hyle_bonsai_runner::run_bonsai(&payload.elf, payload.input_data)
         .await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 

crates/client-sdk/Cargo.toml
@@ -1,5 +1,6 @@
 [package]
-name = "client-sdk"
+name = "hyle-client-sdk"
+description = "Hyle client SDK"
 version = { workspace = true }
 edition = { workspace = true }
 # license = { workspace = true }
@@ -8,7 +9,7 @@ repository = { workspace = true }
 rust-version = { workspace = true }
 
 [dependencies]
-sdk = { path = "../contract-sdk", package = "hyle-contract-sdk", default-features = false, features = [
+sdk = { workspace = true, default-features = false, features = [
     "full-model",
 ] }
 anyhow = "1.0.96"
@@ -17,7 +18,7 @@ serde = "1.0"
 serde_json = { version = "1" }
 tracing = "0.1"
 
-bonsai-runner = { path = "../bonsai-runner", optional = true }
+bonsai-runner = { workspace = true, optional = true }
 risc0-zkvm = { version = "1.2.4", default-features = false, optional = true }
 sp1-sdk = { version = "4.1.2", default-features = false, optional = true }
 bincode = { version = "1.3.3", optional = true }

crates/contract-sdk/Cargo.toml
@@ -1,5 +1,6 @@
 [package]
 name = "hyle-contract-sdk"
+description = "Hyle smart contract SDK"
 version = { workspace = true }
 edition = { workspace = true }
 # license = { workspace = true }
@@ -8,7 +9,7 @@ repository = { workspace = true }
 rust-version = "1.81"
 
 [dependencies]
-hyle_model = { path = "../hyle-model", package = "hyle-model", default-features = false }
+hyle-model = { workspace = true, default-features = false }
 serde = { version = "1.0", default-features = false, features = [
     "derive",
     "alloc",
@@ -26,4 +27,4 @@ mockall = "0.13.0"
 risc0 = ["dep:risc0-zkvm"]
 sp1 = ["dep:sp1-zkvm"]
 tracing = ["dep:tracing"]
-full-model = ["hyle_model/full"]
+full-model = ["hyle-model/full"]

crates/contracts/Cargo.toml
@@ -12,7 +12,7 @@ rust-version = "1.81"
 path = "metadata.rs"
 
 [dependencies]
-sdk = { path = "../contract-sdk", package = "hyle-contract-sdk" }
+sdk = { workspace = true }
 
 [build-dependencies]
 risc0-build = { version = "1.2.4", optional = true }

crates/contracts/amm/Cargo.toml
@@ -1,5 +1,6 @@
 [package]
-name = "amm"
+name = "hyle-amm"
+description = "Hyle Smart Contract"
 version = { workspace = true }
 edition = { workspace = true }
 # license = { workspace = true }
@@ -9,11 +10,12 @@ rust-version = "1.81"
 
 [[bin]]
 name = "amm"
+path = "src/main.rs"
 required-features = ["risc0"]
 test = false
 
 [dependencies]
-sdk = { path = "../../contract-sdk", package = "hyle-contract-sdk" }
+sdk = { workspace = true }
 serde = { version = "1.0", default-features = false, features = [
     "derive",
     "alloc",
@@ -24,11 +26,9 @@ borsh = { version = "1.5.5", features = ["derive"] }
 risc0-zkvm = { version = "1.2.4", default-features = false, optional = true, features = [
     'std',
 ] }
-client_sdk = { path = "../../client-sdk", package = "client-sdk", features = [
-    "risc0",
-], optional = true }
+client-sdk = { workspace = true, features = ["risc0"], optional = true }
 
 [features]
 default = []
-client = ["dep:client_sdk"]
+client = ["dep:client-sdk"]
 risc0 = ["dep:risc0-zkvm", "sdk/risc0"]

commit 40d537b618e52914ee785c2fa70f603c384fc23e

🔧 Fix contract build
broken by #837

crates/contracts/build.rs
@@ -18,12 +18,12 @@ fn main() {
     let mut options = HashMap::new();
 
     [
-        "amm",
-        "hydentity",
-        "hyllar",
-        "staking",
-        "risc0-recursion",
-        "uuid-tld",
+        "hyle-amm",
+        "hyle-hydentity",
+        "hyle-hyllar",
+        "hyle-staking",
+        "hyle-risc0-recursion",
+        "hyle-uuid-tld",
     ]
     .iter()
     .for_each(|name| {

crates/contracts/risc0-recursion/src/bin/guest.rs
@@ -4,13 +4,13 @@
 extern crate alloc;
 
 use alloc::vec::Vec;
-use hyle_risc0_recursion::{Risc0Journal, Risc0ProgramId};
+use hyle_risc0_recursion::{ProofInput, Risc0Journal, Risc0ProgramId};
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 
 fn main() {
-    let mut input: Vec<risc0_recursion::ProofInput> = env::read();
+    let mut input: Vec<ProofInput> = env::read();
     let mut outputs: Vec<(Risc0ProgramId, Risc0Journal)> = Vec::new();
     input.iter_mut().for_each(|input| {
         risc0_zkvm::guest::env::verify(input.image_id, &input.journal)