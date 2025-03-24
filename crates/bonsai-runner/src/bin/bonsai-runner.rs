b98e020e957d291fbf216e3b937964dd5ef24135
Alexandre Careil <alex.careil.fr@gmail.com>
2025-03-24 16:07:44 +0100

    WIP

--- crates/bonsai-runner/src/bin/bonsai-runner.rs
+++ crates/bonsai-runner/src/bin/bonsai-runner.rs
@@ -33,9 +33,7 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     info!("Server listening on {}", addr);
 
-    let listener = hyle_net::net::TcpListener::bind("0.0.0.0:3000")
-        .await
-        .unwrap();
+    let listener = hyle_net::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
--- crates/hyle-net/src/net.rs
+++ crates/hyle-net/src/net.rs
@@ -1,1 +1,7 @@
+#[cfg(not(feature = "turmoil"))]
 pub use tokio::net::*;
+
+#[cfg(feature = "turmoil")]
+pub use turmoil::net::*;
+#[cfg(feature = "turmoil")]
+pub use turmoil::*;
