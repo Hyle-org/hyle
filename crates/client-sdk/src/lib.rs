c6f33330f875a3e0af542cf41510f945f33fd1fa
Alexandre Careil <alex.careil.fr@gmail.com>
2025-03-24 11:28:33 +0100

    Move tcp client server for tcp in hyle-net + remove tcp feature in client-sdk

--- crates/client-sdk/src/lib.rs
+++ crates/client-sdk/src/lib.rs
@@ -3,6 +3,5 @@ pub mod contract_indexer;
 pub mod helpers;
 #[cfg(feature = "rest")]
 pub mod rest_client;
-#[cfg(feature = "tcp")]
 pub mod tcp_client;
 pub mod transaction_builder;
--- crates/client-sdk/src/tcp_client.rs
+++ crates/client-sdk/src/tcp_client.rs
@@ -1,15 +1,1 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use sdk::Transaction;
-
-#[derive(Debug, Clone, BorshSerialize, BorshDeserialize, Eq, PartialEq)]
-pub enum TcpServerMessage {
-    NewTx(Transaction),
-}
-#[derive(Debug, Clone, BorshSerialize, BorshDeserialize, Eq, PartialEq)]
-pub struct TcpServerResponse;
-
-hyle_net::tcp_client_server! {
-    pub TcpServer,
-    request: TcpServerMessage,
-    response: TcpServerResponse
-}
+pub use hyle_net::api::tcp::*;
