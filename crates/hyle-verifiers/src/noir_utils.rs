aeeeca0557e80b1eb817b8f7aae787bcadf96d22
Bertrand Darbon <bertrand@hyle.eu>
2025-04-02 17:06:05 +0200

    update noir parser

--- crates/hyle-verifiers/src/noir_utils.rs
+++ crates/hyle-verifiers/src/noir_utils.rs
@@ -10,7 +10,7 @@ pub fn parse_noir_output(vector: &mut Vec<String>) -> Result<HyleOutput, Error> 
     let identity = parse_string(vector)?;
     let tx_hash = parse_string(vector)?;
     let index = u32::from_str_radix(vector.remove(0).strip_prefix("0x").context("parsing")?, 16)?;
-    let _blobs = parse_blobs(vector)?;
+    let blobs = parse_blobs(vector)?;
     let success =
         u32::from_str_radix(vector.remove(0).strip_prefix("0x").context("parsing")?, 16)? == 1;
 
@@ -22,7 +22,7 @@ pub fn parse_noir_output(vector: &mut Vec<String>) -> Result<HyleOutput, Error> 
         tx_hash: TxHash(tx_hash),
         tx_ctx: None,
         index: BlobIndex(index as usize),
-        blobs: vec![], // TODO
+        blobs,
         success,
         onchain_effects: vec![],
         program_outputs: vec![],
@@ -54,7 +54,7 @@ fn parse_array(vector: &mut Vec<String>) -> Result<Vec<u8>, Error> {
     Ok(resp)
 }
 
-fn parse_blobs(vector: &mut Vec<String>) -> Result<Vec<u8>, Error> {
+fn parse_blobs(vector: &mut Vec<String>) -> Result<Vec<(BlobIndex, Vec<u8>)>, Error> {
     let _blob_len =
         usize::from_str_radix(vector.remove(0).strip_prefix("0x").context("parsing")?, 16)?;
     let mut blob_data = VecDeque::from(vector.clone());
@@ -70,6 +70,9 @@ fn parse_blobs(vector: &mut Vec<String>) -> Result<Vec<u8>, Error> {
     let mut blobs = Vec::new();
 
     for _ in 0..blob_number {
+        let index =
+            u32::from_str_radix(vector.remove(0).strip_prefix("0x").context("parsing")?, 16)?;
+
         let blob_size = usize::from_str_radix(
             blob_data
                 .pop_front()
@@ -79,15 +82,18 @@ fn parse_blobs(vector: &mut Vec<String>) -> Result<Vec<u8>, Error> {
             16,
         )?;
 
+        let mut blob = Vec::with_capacity(blob_size);
+
         for _ in 0..blob_size {
             let v = &blob_data
                 .pop_front()
                 .ok_or_else(|| anyhow::anyhow!("Missing blob data"))?;
-            blobs.push(u8::from_str_radix(
+            blob.push(u8::from_str_radix(
                 v.strip_prefix("0x").context("parsing")?,
                 16,
             )?);
         }
+        blobs.push((BlobIndex(index as usize), blob));
     }
 
     Ok(blobs)
