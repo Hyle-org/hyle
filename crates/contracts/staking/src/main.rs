38bf56759400970521284690476bbbad421aed0c
Bertrand Darbon <bertrand@hyle.eu>
2025-04-22 17:00:52 +0200

    rename

--- crates/contracts/staking/src/main.rs
+++ crates/contracts/staking/src/main.rs
@@ -14,8 +14,8 @@ risc0_zkvm::guest::entry!(main);
 
 fn main() {
     let env = Risc0Env {};
-    let (commitment_metadata, calldata): (Vec<u8>, Vec<Calldata>) = env.read();
+    let (commitment_metadata, calldatas): (Vec<u8>, Vec<Calldata>) = env.read();
 
-    let output = execute::<Staking>(&commitment_metadata, &calldata);
+    let output = execute::<Staking>(&commitment_metadata, &calldatas);
     env.commit(output);
 }
