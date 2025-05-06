01e1a6349977e5e19dee7267c1aec896114420f9
Bertrand Darbon <bertrand@hyle.eu>
2025-05-06 17:04:15 +0200

    parse vec<HyleOuput>

--- crates/hyle-verifiers/src/lib.rs
+++ crates/hyle-verifiers/src/lib.rs
@@ -217,8 +217,17 @@ pub mod sp1_4 {
             .context("SP1 proof verification failed")?;
 
         // TODO: support multi-output proofs.
-        let hyle_output = borsh::from_slice::<HyleOutput>(proof.public_values.as_slice())
-            .context("Failed to extract HyleOuput from SP1 proof")?;
+        let hyle_output = match borsh::from_slice::<Vec<HyleOutput>>(proof.public_values.as_slice())
+        {
+            Ok(outputs) => outputs,
+            Err(_) => {
+                debug!("Failed to decode Vec<HyleOutput>, trying to decode as HyleOutput");
+                vec![
+                    borsh::from_slice::<HyleOutput>(proof.public_values.as_slice())
+                        .context("Failed to extract HyleOuput from SP1 proof")?,
+                ]
+            }
+        };
 
         tracing::info!("✅ SP1 proof verified.",);
 
